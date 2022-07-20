d66d7b8bcd0e34664d9774ad151dda3cfcd5939c
JohnFranclin <61628402+JohnFranclin@users.noreply.github.com>
2022-07-20 11:30:59 +0200

    Initial commit + structure cleanup

---
+++ app.js
@@ -0,0 +1,51 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const playerRouter = require('./routes/player');
+const searchRouter = require('./routes/search');
+const penaltiesRouter = require('./routes/penalties');
+const chatRouter = require('./routes/chat');
+const serversRouter = require('./routes/servers');
+const ipRouter = require('./routes/ip');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/player', playerRouter);
+app.use('/search', searchRouter);
+app.use('/penalties', penaltiesRouter);
+app.use('/chat', chatRouter);
+app.use('/servers', serversRouter);
+app.use('/ip', ipRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ dbconfig.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        /* don't expose password or any sensitive info, done only for demo */
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "ebc_b3_pm",
+    },
+    listPerPage: 10,
+};
+module.exports = config;
